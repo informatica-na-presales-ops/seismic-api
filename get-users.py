4e2e67451a7ad578deb0103aaecd0b2a47a2c004
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-05-22 10:19:04 -0500

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -1,5 +1,6 @@
 import apscheduler.schedulers.blocking
 import datetime
+import itertools
 import notch
 import os
 import psycopg2.extras
@@ -11,32 +12,79 @@ import uuid
 log = notch.make_log('seismic_api.get_users')
 
 
-def batch_upsert_records(cur, records):
+def batch_upsert_user_property_assignments(cur, records):
     sql = '''
-        insert into seismic_users_raw (
-            id, created_at, default_content_profile_id, default_content_profile_name, deleted_at, email,
-            email_domain, first_name, full_name, is_deleted, is_seismic_employee, is_system_admin,
-            last_name, license_type, modified_at, organization, sso_user_id, title, username,
-            is_locked, address, phone_number, latest_activity_date, external_id
+        insert into seismic_user_property_assignments (
+            _synced, modified_at, user_id, user_property_id, user_property_name, user_property_type,
+            user_property_value, user_property_value_boolean, user_property_value_datetime,
+            user_property_value_float, user_property_value_integer
         ) values (
-            %(id)s, %(createdAt)s, %(defaultContentProfileId)s, %(defaultContentProfileName)s, %(deletedAt)s, %(email)s,
-            %(emailDomain)s, %(firstName)s, %(fullName)s, %(isDeleted)s, %(isSeismicEmployee)s, %(isSystemAdmin)s,
-            %(lastName)s, %(licenseType)s, %(modifiedAt)s, %(organization)s, %(ssoUserId)s, %(title)s, %(username)s,
-            %(isLocked)s, %(address)s, %(phoneNumber)s, %(latestActivityDate)s, %(externalId)s
-        ) on conflict (id) do update set
-            created_at = %(createdAt)s, default_content_profile_id = %(defaultContentProfileId)s,
-            default_content_profile_name = %(defaultContentProfileName)s, deleted_at = %(deletedAt)s, email = %(email)s,
-            email_domain = %(emailDomain)s, first_name = %(firstName)s, full_name = %(fullName)s,
-            is_deleted = %(isDeleted)s, is_seismic_employee = %(isSeismicEmployee)s,
-            is_system_admin = %(isSystemAdmin)s, last_name = %(lastName)s, license_type = %(licenseType)s,
-            modified_at = %(modifiedAt)s, organization = %(organization)s, sso_user_id = %(ssoUserId)s,
-            title = %(title)s, username = %(username)s, is_locked = %(isLocked)s, address = %(address)s,
-            phone_number = %(phoneNumber)s, latest_activity_date = %(latestActivityDate)s, external_id = %(externalId)s
+            true, %(modifiedAt)s, %(userId)s, %(userPropertyId)s, %(userPropertyName)s, %(userPropertyType)s,
+            %(userPropertyValue)s, %(userPropertyValueBoolean)s, %(userPropertyValueDatetime)s,
+            %(userPropertyValueFloat)s, %(userPropertyValueInteger)s
+        ) on conflict (user_id, user_property_id) do update set
+            _synced = true, modified_at = excluded.modified_at, user_property_name = excluded.user_property_name,
+            user_property_type = excluded.user_property_type, user_property_value = excluded.user_property_value,
+            user_property_value_boolean = excluded.user_property_value_boolean,
+            user_property_value_datetime = excluded.user_property_value_datetime,
+            user_property_value_float = excluded.user_property_value_float,
+            user_property_value_integer = excluded.user_property_value_integer
     '''
     plural = 's'
     if len(records) == 1:
         plural = ''
-    log.info(f'Saving {len(records)} record{plural} to database')
+    log.info(f'Saving {len(records)} user property assignment{plural} to database')
+    psycopg2.extras.execute_batch(cur, sql, records)
+
+
+def user_property_assignments_sync_begin(cur):
+    sql = '''
+        update seismic_user_property_assignments
+        set _synced = false
+        where _synced is null or _synced is true
+    '''
+    cur.execute(sql)
+
+
+def user_property_assignments_sync_end(cur):
+    sql = '''
+        delete from seismic_user_property_assignments
+        where _synced is false
+    '''
+    cur.execute(sql)
+
+
+def batch_upsert_users(cur, records):
+    sql = '''
+        insert into seismic_users_raw (
+            id, created_at, default_content_profile_id, default_content_profile_name, deleted_at, email,
+            email_domain, first_name, full_name, is_deleted, is_lessonly_enabled, is_seismic_employee,
+            is_seismic_enabled, is_system_admin, last_name, license_type, modified_at, organization,
+            sso_user_id, title, username, is_locked, address, phone_number, latest_activity_date,
+            external_id
+        ) values (
+            %(id)s, %(createdAt)s, %(defaultContentProfileId)s, %(defaultContentProfileName)s, %(deletedAt)s, %(email)s,
+            %(emailDomain)s, %(firstName)s, %(fullName)s, %(isDeleted)s, %(isLessonlyEnabled)s, %(isSeismicEmployee)s,
+            %(isSeismicEnabled)s, %(isSystemAdmin)s, %(lastName)s, %(licenseType)s, %(modifiedAt)s, %(organization)s,
+            %(ssoUserId)s, %(title)s, %(username)s, %(isLocked)s, %(address)s, %(phoneNumber)s, %(latestActivityDate)s,
+            %(externalId)s
+        ) on conflict (id) do update set
+            created_at = excluded.created_at, default_content_profile_id = excluded.default_content_profile_id,
+            default_content_profile_name = excluded.default_content_profile_name, deleted_at = excluded.deleted_at,
+            email = excluded.email, email_domain = excluded.email_domain, first_name = excluded.first_name,
+            full_name = excluded.full_name, is_deleted = excluded.is_deleted,
+            is_lessonly_enabled = excluded.is_lessonly_enabled, is_seismic_employee = excluded.is_seismic_employee,
+            is_seismic_enabled = excluded.is_seismic_enabled, is_system_admin = excluded.is_system_admin,
+            last_name = excluded.last_name, license_type = excluded.license_type, modified_at = excluded.modified_at,
+            organization = excluded.organization, sso_user_id = excluded.sso_user_id, title = excluded.title,
+            username = excluded.username, is_locked = excluded.is_locked, address = excluded.address,
+            phone_number = excluded.phone_number, latest_activity_date = excluded.latest_activity_date,
+            external_id = excluded.external_id
+    '''
+    plural = 's'
+    if len(records) == 1:
+        plural = ''
+    log.info(f'Saving {len(records)} user{plural} to database')
     psycopg2.extras.execute_batch(cur, sql, records)
 
 
@@ -49,7 +97,7 @@ def get_max_modified_at(cur):
     row = cur.fetchone()
     val = row.get('max_modified_at')
     if val is None:
-        val = datetime.datetime(2020, 1, 1, tzinfo=datetime.timezone.utc)
+        val = datetime.datetime(2018, 1, 1, tzinfo=datetime.timezone.utc)
     return val
 
 
@@ -82,9 +130,21 @@ def main_job(repeat_interval_hours: int = None):
 
         with cnx:
             with cnx.cursor() as cur:
-                batch_upsert_records(cur, records)
+                batch_upsert_users(cur, records)
 
         modified_at_start_time = modified_at_end_time
+
+    log.info('Updating user property assignments')
+    with cnx:
+        with cnx.cursor() as cur:
+            user_property_assignments_sync_begin(cur)
+    for batch in itertools.batched(c.user_propert_assignments(), 3000):
+        with cnx:
+            with cnx.cursor() as cur:
+                batch_upsert_user_property_assignments(cur, batch)
+    with cnx:
+        with cnx.cursor() as cur:
+            user_property_assignments_sync_end(cur)
 
     if repeat_interval_hours:
         plural = 's'
